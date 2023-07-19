18a9751bed831be8689a2be1d4f33a8f07722955
Juan Ignacio Benito <benitojuanignacio@gmail.com>
2023-07-19 14:19:40 +0100

    Fixed naming

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
-import './App.css';
-import './complements/style.css'
+import "./App.css";
+import "./components/style.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Login } from "./components/Login";
+import { Register } from "./components/Register";
 
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-
-import {Login} from './complements/Login';
-import {Register} from './complements/Register';
-import {Home} from './complements/Home';
-
-import {AuthProvider} from './context/authContext';
-
-
+import { AuthProvider } from "./context/authContext";
+import { Home } from "./components/Home";
 
 function App() {
   return (
-
     <AuthProvider>
-
       <Router>
         <Routes>
-          <Route path='/' element={<Home/>}/> 
-          <Route path='/login' element={<Login/>}/> 
-          <Route path='/register' element={<Register/>}/> 
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </Router>
-
     </AuthProvider>
   );
 }
